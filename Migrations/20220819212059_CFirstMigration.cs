f64f018251d5d18bb801b2dc478861b43c42d19c
Cadams3443 <adams.cody.biz@gmail.com>
2022-08-19 14:21:53 -0700

    Migrations

---
+++ Migrations/20220819212059_CFirstMigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LipService.Migrations
+{
+    public partial class CFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
